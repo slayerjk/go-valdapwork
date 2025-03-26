57ef888ef62a43b40452f36a286687cebb09a1cb
slayerjk <slayer.jk@mail.ru>
2025-03-26 15:18:46 +0500

    + main.go(usage examples); * heavy reactored of package;

--- valdapwork.go
+++ valdapwork.go
@@ -1,86 +1,115 @@
 package valdapwork
 
 import (
+	"crypto/tls"
 	"fmt"
 
-	"github.com/go-ldap/ldap"
+	"github.com/go-ldap/ldap/v3"
 )
 
-// Make LDAP connection
-func ldapConnect(ldapFqdn string) (*ldap.Conn, error) {
-	conn, err := ldap.DialURL(fmt.Sprintf("ldap://%s:389", ldapFqdn))
+// Make LDAP connection(without TLS), using Domain's FQDN & LDAP's common port(389)
+func MakeLdapConnection(ldapFqdn string) (*ldap.Conn, error) {
+	// defining connection URL: ldap/ldaps
+	connURL := fmt.Sprintf("ldap://%s:389", ldapFqdn)
+
+	// dial URL
+	conn, err := ldap.DialURL(connURL)
 	if err != nil {
 		return nil, err
 	}
 
-	return conn, nil
-}
-
-// Search ONLY Enabled user's 'samaccountname' by it's 'displayname'
-//
-// filterName - prefix of samaAccountName to exclude from result, use "" if don't need it;
-// also exclude disabled accounts (userAccountControl != 546|514);
-// dnFilter - some text of full DN to INCLUDE even if account is disabled, use "" if don't need it;
-func BindAndSearchSamaccountnameByDisplayname(userAcc, ldapFqdn, ldapBasedn, bindUser, bindPass, filterSama string) (string, error) {
-	var result string
-
-	// forming LDAP filter; use exclude prefix if len(filterSama) > 0
-	filter := fmt.Sprintf("(&(objectClass=user)(displayname=%s))", userAcc)
-	if len(filterSama) > 0 {
-		filter = fmt.Sprintf("(&(objectClass=user)(displayname=%s)(!samaccountname=%s*))", userAcc, filterSama)
-	}
-
-	// make LDAP connection
-	conn, err := ldapConnect(ldapFqdn)
-	if err != nil {
-		return "", err
-	}
-	defer conn.Close()
-
 	// if debug level neede
 	// conn.Debug = true
 
-	// make LDAP bind
-	errBind := conn.Bind(bindUser, bindPass)
-	if errBind != nil {
-		return "", fmt.Errorf("failed to make ldap bind:\n\t%v", errBind)
+	return conn, nil
+}
+
+// Make LDAP TLS Connection with existing LDAP connection
+// Start connect with default ldap conn(389) then reconnect to use TLS
+func StartTLSConnWoVerification(conn *ldap.Conn) error {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
 	}
 
-	// forming LDAP search request for 'samaccountname' and 'useraccountcontrol'(disabled=546)
+	// reDial with TLS
+	err := conn.StartTLS(tlsConfig)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Make LDAP Bind
+func LdapBind(conn *ldap.Conn, bindUser, bindPassword string) error {
+	err := conn.Bind(bindUser, bindPassword)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Make LDAP search request based on LDAP filter & LDAP Attributes to get
+// Example of filter: "(&(objectClass=user)(samaccountname=%s))"
+func MakeSearchReq(conn *ldap.Conn, ldapBaseDN string, ldapFilter string, ldapAttrs ...string) ([]*ldap.Entry, error) {
 	searchReq := ldap.NewSearchRequest(
-		ldapBasedn,
+		ldapBaseDN,
 		ldap.ScopeWholeSubtree,
 		ldap.NeverDerefAliases,
 		0,
 		0,
 		false,
-		filter,
-		[]string{"samaccountname", "useraccountcontrol"},
+		ldapFilter,
+		ldapAttrs,
 		nil,
 	)
 
 	// making LDAP search request
 	conResult, err := conn.Search(searchReq)
 	if err != nil {
-		return "", fmt.Errorf("failed to make ldap search:\n\t%v", err)
+		return nil, err
 	}
 
 	// check if result is empty
 	if len(conResult.Entries) == 0 {
-		return "", fmt.Errorf("failed to find any entry, empty result")
+		return nil, fmt.Errorf("empty result")
+	}
+
+	return conResult.Entries, nil
+}
+
+// Search ONLY Enabled user's 'samaccountname' by it's 'displayname'
+//
+// filterName - PREFIX of samaAccountName to exclude from result, use "" if don't need it;
+// also exclude disabled accounts (userAccountControl != 546|514);
+// dnFilter - some text of full DN to INCLUDE even if account is disabled, use "" if don't need it;
+func SearchEnabledSAMAByDisplayname(conn *ldap.Conn, displayName, ldapBasedn, filterSama string) (string, error) {
+	var result string
+
+	// forming LDAP filter; use exclude prefix if len(filterSama) > 0
+	filter := fmt.Sprintf("(&(objectClass=user)(displayname=%s))", displayName)
+	if len(filterSama) > 0 {
+		filter = fmt.Sprintf("(&(objectClass=user)(displayname=%s)(!samaccountname=%s*))", displayName, filterSama)
+	}
+
+	// forming LDAP search request for 'samaccountname' and 'useraccountcontrol'(disabled=546)
+	searchResult, err := MakeSearchReq(conn, ldapBasedn, filter, "samaccountname", "useraccountcontrol")
+	if err != nil {
+		return "", fmt.Errorf("failed to make LDAP search:\n\t%v", err)
 	}
 
 	// check if len(conResult.Entries) > 1, choose only enabled(userAccountControl != 546)
-	if len(conResult.Entries) >= 1 {
-		for ind := range conResult.Entries {
+	if len(searchResult) >= 1 {
+		for ind := range searchResult {
 			// pretty print
 			// conResult.Entries[ind].PrettyPrint(4)
 
 			// choose first enabled account as result
-			userAccountControl := conResult.Entries[ind].GetAttributeValue("userAccountControl")
+			userAccountControl := searchResult[ind].GetAttributeValue("userAccountControl")
 
 			if userAccountControl != "546" && userAccountControl != "514" {
-				result = conResult.Entries[ind].GetAttributeValue("sAMAccountName")
+				result = searchResult[ind].GetAttributeValue("sAMAccountName")
 
 				if len(result) == 0 {
 					return "", fmt.Errorf("failed to find 'sAMAccountName' attribute value, empty result")
